2960e98d937020aa5b857f79d0982070149d7b84
evgsbel <beloff163@gmail.com>
2020-05-08 00:27:50 +0400

    add modal cart, finish order page

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,7 @@
 //         $('.preloader__wrp').fadeOut();
 //     });
 // });
+
 $(document).ready(function () {
     let arrow_prev = "<svg width='11' height='10' viewBox='0 0 11 10' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M11 5H2M2 5L5.5 1.5M2 5L5.5 8.5' stroke='#2A2A2A' stroke-width='2'/></svg>",
         arrow_next = "<svg width='11' height='10' viewBox='0 0 11 10' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M0 5H9M9 5L5.5 1.5M9 5L5.5 8.5' stroke='#2A2A2A' stroke-width='2'/></svg>",
@@ -29,7 +30,7 @@ $(document).ready(function () {
         margin: 20,
         dotsData: true,
         nav: true,
-        URLhashListener:true,
+        URLhashListener: true,
         dots: true,
         navText: [arrow_prev, arrow_next],
         navClass: ["slider-arrow slider-arrow_prev products number-arrow number-arrow_prev", "slider-arrow slider-arrow_next products number-arrow number-arrow_next"],
@@ -58,7 +59,7 @@ $(document).ready(function () {
         margin: 18,
         dotsData: true,
         nav: true,
-        URLhashListener:true,
+        URLhashListener: true,
         dots: true,
         navText: [arrow_prev, arrow_next],
         navClass: ["slider-arrow slider-arrow_prev products number-arrow number-arrow_prev", "slider-arrow slider-arrow_next products number-arrow number-arrow_next"],
@@ -87,7 +88,7 @@ $(document).ready(function () {
         loop: true,
         dotsData: true,
         nav: true,
-        URLhashListener:true,
+        URLhashListener: true,
         dots: true,
         navText: [arrow_prev, arrow_next_white],
         navClass: ["slider-arrow slider-arrow_prev products number-arrow number-arrow_prev news-arrow news-arrow_prev", "slider-arrow slider-arrow_next products number-arrow number-arrow_next news-arrow news-arrow_next"],
@@ -103,7 +104,7 @@ $(document).ready(function () {
         margin: 20,
         dotsData: true,
         nav: true,
-        URLhashListener:true,
+        URLhashListener: true,
         dots: true,
         navText: [arrow_prev, arrow_next],
         navClass: ["slider-arrow slider-arrow_prev products number-arrow number-arrow_prev", "slider-arrow slider-arrow_next products number-arrow number-arrow_next"],
@@ -120,12 +121,12 @@ $(document).ready(function () {
         }
     });
 
-     $('.js-works-slider').owlCarousel({
+    $('.js-works-slider').owlCarousel({
         items: 1,
         loop: true,
         dotsData: true,
         nav: true,
-        URLhashListener:true,
+        URLhashListener: true,
         dots: true,
         navText: [arrow_prev, arrow_next],
         navClass: ["slider-arrow slider-arrow_prev products number-arrow number-arrow_prev", "slider-arrow slider-arrow_next products number-arrow number-arrow_next"],
@@ -133,17 +134,17 @@ $(document).ready(function () {
         dotsClass: 'dots-number',
         dotClass: 'dots-number__item',
     });
-$(".js-smalimg-slider").owlCarousel({
-    items: 3,
-    loop: false,
-    mouseDrag: false,
-    touchDrag: false,
-    pullDrag: false,
-    rewind: true,
-    autoplay: true,
-    margin: 0,
-    nav: true
-});
+    $(".js-smalimg-slider").owlCarousel({
+        items: 3,
+        loop: false,
+        mouseDrag: false,
+        touchDrag: false,
+        pullDrag: false,
+        rewind: true,
+        autoplay: true,
+        margin: 0,
+        nav: true
+    });
 
 });
 
@@ -151,9 +152,9 @@ $(".js-smalimg-slider").owlCarousel({
 $(() => {
     $('.hamburger').on('click', function headerHambClick() {
         $(this).toggleClass('is-active')
-         $('.mobile-menu')
+        $('.mobile-menu')
             .slideToggle()
-             .toggleClass('mobile-menu_open')
+            .toggleClass('mobile-menu_open')
         //     .removeClass('feed-open');
     });
 });
@@ -170,40 +171,51 @@ $('.js-select').select2({
 });
 
 // input number
-$(function() {
+$(function () {
 
-  (function quantityProducts() {
-    let $quantityArrowMinus = $(".input-number__btn_minus");
-    let $quantityArrowPlus = $(".input-number__btn_plus");
-    let $quantityNum = $(".input-number__input");
+    (function quantityProducts() {
+        let $quantityArrowMinus = $(".input-number__btn_minus");
+        let $quantityArrowPlus = $(".input-number__btn_plus");
+        let $quantityNum = $(".input-number__input");
 
-    $quantityArrowMinus.click(quantityMinus);
-    $quantityArrowPlus.click(quantityPlus);
+        $quantityArrowMinus.click(quantityMinus);
+        $quantityArrowPlus.click(quantityPlus);
 
-    function quantityMinus() {
-      if ($quantityNum.val() > 1) {
-        $quantityNum.val(+$quantityNum.val() - 1);
-      }
+        function quantityMinus() {
+            if ($quantityNum.val() > 1) {
+                $quantityNum.val(+$quantityNum.val() - 1);
+            }
+        }
+
+        function quantityPlus() {
+            $quantityNum.val(+$quantityNum.val() + 1);
+        }
+    })();
+
+});
+
+$('[data-fancybox]').fancybox({
+    btnTpl: {
+        smallBtn:
+            '<button type="button" data-fancybox-close class="modal-cart__close" title="{{CLOSE}}">' + 'Закрыть' +
+            '<svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">' +
+            '<path d="M1.42553 1.42553L21 21M1 20.5745L20.5745 1" stroke="#2A2A2A" stroke-width="2"/>' +
+            '</svg>' +
+            "</button>"
     }
-
-    function quantityPlus() {
-      $quantityNum.val(+$quantityNum.val() + 1);
-    }
-  })();
-
 });
 
 // tabs
 const tabs = document.querySelector(".tabs");
-//const activeBtns = document.querySelectorAll(".is-active");
 const tabsBtns = tabs.querySelectorAll(".tabs__btn");
 const tabsContents = tabs.querySelectorAll(".tabs__content");
 
 function displayCurrentTab(current) {
-  for (let i = 0; i < tabsContents.length; i++) {
-    tabsContents[i].style.display = (current === i) ? "block" : "none";
-  }
+    for (let i = 0; i < tabsContents.length; i++) {
+        tabsContents[i].style.display = (current === i) ? "block" : "none";
+    }
 }
+
 displayCurrentTab(0);
 
 
@@ -211,14 +223,15 @@ tabs.addEventListener("click", event => {
     for (let i = 0; i < tabsBtns.length; i++) {
         tabsBtns[i].classList.remove("is-active");
     }
-  if (event.target.className === "tabs__btn") {
-    for (let i = 0; i < tabsBtns.length; i++) {
-      if (event.target === tabsBtns[i]) {
+    if (event.target.className === "tabs__btn") {
+        for (let i = 0; i < tabsBtns.length; i++) {
+            if (event.target === tabsBtns[i]) {
 
-        displayCurrentTab(i);
-        event.target.classList.add('is-active');
-        break;
-      }
+                displayCurrentTab(i);
+                event.target.classList.add('is-active');
+                break;
+            }
+        }
     }
-  }
 });
+
