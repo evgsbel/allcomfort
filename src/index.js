22117d364d9bbc102a4e5b49bb3be14d3f90bcc5
evgsbel <beloff163@gmail.com>
2020-05-13 16:24:56 +0400

    fix node ver.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'bootstrap'
 import 'owl.carousel';
 import '@fancyapps/fancybox'
 import 'select2/dist/js/select2.min'
+import symbol from './assets/img/twitter.svg'
 import './js/app'
 
 // SCSS
--- webpack.config.js
+++ webpack.config.js
@@ -10,144 +10,152 @@ const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 // Main const. Feel free to change it
 const PATHS = {
-  src: path.join(__dirname, "/src"),
-  dist: path.join(__dirname, "/dist"),
-  assets: "assets/"
+    src: path.join(__dirname, "/src"),
+    dist: path.join(__dirname, "/dist"),
+    assets: "assets/"
 };
 
 // Pages const for HtmlWebpackPlugin
 // see more: https://github.com/vedees/webpack-template/blob/master/README.md#html-dir-folder
 const PAGES_DIR = PATHS.src;
 const PAGES = fs
-  .readdirSync(PAGES_DIR)
-  .filter(fileName => fileName.endsWith(".html"));
+    .readdirSync(PAGES_DIR)
+    .filter(fileName => fileName.endsWith(".html"));
 
 module.exports = {
-  externals: {
-    paths: PATHS
-  },
-  entry: {
-    app: PATHS.src
-    // module: `${PATHS.src}/your-module.js`,
-  },
-  output: {
-    filename: `${PATHS.assets}js/[name].[contenthash].js`,
-    path: PATHS.dist,
-    //publicPath: "./"
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          name: "vendors",
-          test: /node_modules/,
-          chunks: "all",
-          enforce: true
-        }
-      }
-    }
-  },
-  module: {
-    rules: [
-      {
-        // JavaScript
-        test: /\.js$/,
-        loader: "babel-loader",
-        exclude: "/node_modules/"
-      },
-      {
-        // Fonts
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: "file-loader",
-        options: {
-          name: "../fonts/[name].[ext]"
-        }
-      },
-      {
-        // images / icons
-        test: /\.(png|jpg|gif|svg)$/,
-        loader: "file-loader",
-        options: {
-          name: "../img/[name].[ext]"
-        }
-      },
-      {
-        // scss
-        test: /\.s[ac]ss$/,
-        use: [
-          "style-loader",
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: { sourceMap: true }
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              sourceMap: true,
-              config: { path: `./postcss.config.js` }
+    externals: {
+        paths: PATHS
+    },
+    entry: {
+        app: PATHS.src
+        // module: `${PATHS.src}/your-module.js`,
+    },
+    output: {
+        filename: `${PATHS.assets}js/[name].[contenthash].js`,
+        path: PATHS.dist,
+        //publicPath: "./"
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                vendor: {
+                    name: "vendors",
+                    test: /node_modules/,
+                    chunks: "all",
+                    enforce: true
+                }
             }
-          },
-          {
-            loader: "sass-loader",
-            options: { sourceMap: true }
-          }
-        ]
-      },
-      {
-        // css
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          MiniCssExtractPlugin.loader,
-          {
-            loader: "css-loader",
-            options: { sourceMap: true }
-          },
-          {
-            loader: "postcss-loader",
-            options: {
-              sourceMap: true,
-              config: { path: `./postcss.config.js` }
+        }
+    },
+    module: {
+        rules: [
+            {
+                // JavaScript
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: "/node_modules/"
+            },
+            {
+                // Fonts
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: "file-loader",
+                options: {
+                    name: "../fonts/[name].[ext]"
+                }
+            },
+            {
+                // images / icons
+                test: /\.(png|jpg|gif|svg)$/,
+                loader: "file-loader",
+                options: {
+                    name: "../img/[name].[ext]"
+                }
+            },
+            {
+                // scss
+                test: /\.s[ac]ss$/,
+                use: [
+                    "style-loader",
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {sourceMap: true}
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: true,
+                            config: {path: `./postcss.config.js`}
+                        }
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {sourceMap: true}
+                    }
+                ]
+            },
+            {
+                // css
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {sourceMap: true}
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            sourceMap: true,
+                            config: {path: `./postcss.config.js`}
+                        }
+                    }
+                ]
+            },
+            //svg
+            {
+                test: /\.svg$/,
+                use: [
+                    'svg-sprite-loader',
+                    'svgo-loader'
+                ]
             }
-          }
         ]
-      }
+    },
+    resolve: {
+        alias: {
+            "~": PATHS.src,
+        }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: `${PATHS.assets}css/[name].[contenthash].css`
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            'window.jQuery': 'jquery'
+        }),
+        new CopyWebpackPlugin([
+            {from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img`},
+            {from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts`},
+            {from: `${PATHS.src}/static`, to: ""}
+        ]),
+        new CleanWebpackPlugin(),
+        /*
+          Automatic creation any html pages (Don't forget to RERUN dev server!)
+          See more:
+          https://github.com/vedees/webpack-template/blob/master/README.md#create-another-html-files
+          Best way to create pages:
+          https://github.com/vedees/webpack-template/blob/master/README.md#third-method-best
+        */
+        ...PAGES.map(
+            page =>
+                new HtmlWebpackPlugin({
+                    template: `${PAGES_DIR}/${page}`,
+                    filename: `./${page}`
+                })
+        )
     ]
-  },
-  resolve: {
-    alias: {
-      "~": PATHS.src,
-    }
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: `${PATHS.assets}css/[name].[contenthash].css`
-    }),
-   new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery'
-    }),
-    new CopyWebpackPlugin([
-      { from: `${PATHS.src}/${PATHS.assets}img`, to: `${PATHS.assets}img` },
-      { from: `${PATHS.src}/${PATHS.assets}fonts`, to: `${PATHS.assets}fonts` },
-      { from: `${PATHS.src}/static`, to: "" }
-    ]),
-    new CleanWebpackPlugin(),
-    /*
-      Automatic creation any html pages (Don't forget to RERUN dev server!)
-      See more:
-      https://github.com/vedees/webpack-template/blob/master/README.md#create-another-html-files
-      Best way to create pages:
-      https://github.com/vedees/webpack-template/blob/master/README.md#third-method-best
-    */
-    ...PAGES.map(
-      page =>
-        new HtmlWebpackPlugin({
-          template: `${PAGES_DIR}/${page}`,
-          filename: `./${page}`
-        })
-    )
-  ]
 };
